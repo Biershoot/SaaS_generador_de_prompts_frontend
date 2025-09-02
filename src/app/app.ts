4f07c3463f788a8eda85e498cbde774e055cd2b6
Alejandro Calderon <alejodim27@gmail.com>
2025-09-02 01:29:39 -0500

    feat(ui): mostrar navbar solo en /generator; ocultarlo en login y otras rutas

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet, NavigationEnd } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { filter } from 'rxjs/operators';
 import { NavbarComponent } from './shared/navbar.component';
 import { AppInitService } from './services/app-init.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [CommonModule, RouterOutlet, NavbarComponent],
   template: `
-    <app-navbar></app-navbar>
+    <app-navbar *ngIf="showNavbar"></app-navbar>
     <main class="main-content">
       <router-outlet />
     </main>
@@ -21,10 +23,22 @@ import { AppInitService } from './services/app-init.service';
   `]
 })
 export class App implements OnInit {
+  currentUrl: string = '';
+  get showNavbar(): boolean {
+    return this.currentUrl.startsWith('/generator');
+  }
   
-  constructor(private appInitService: AppInitService) {}
+  constructor(private appInitService: AppInitService, private router: Router) {}
 
   ngOnInit() {
+    // Actualizar URL actual para controlar visibilidad del navbar
+    this.currentUrl = this.router.url || '';
+    this.router.events
+      .pipe(filter((e): e is NavigationEnd => e instanceof NavigationEnd))
+      .subscribe((e: NavigationEnd) => {
+        this.currentUrl = e.urlAfterRedirects || e.url;
+      });
+
     // Inicializar la aplicación con refresh automático
     this.appInitService.init().subscribe({
       next: (success) => {
